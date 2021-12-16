86e7f7e524f810dff4dc16bc437e2537ba98d95d
Tommy Kovalchuk <tommy.pk17@gmail.com>
2021-12-15 19:41:10 -0500

    testing post-install script, add to parent package.json

---
+++ post-install/index.js
@@ -0,0 +1,13 @@
+const saveFile = require('fs').writeFileSync;
+
+const pkgJsonPath = require.main.paths[0].split('node_modules')[0] + 'package.json';
+
+const json = require(pkgJsonPath);
+
+if (!json.hasOwnProperty('scripts')) {
+    json.scripts = {};
+}
+
+json.scripts['min-css'] = 'node ./node_modules/css-min/index.js --';
+
+saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
