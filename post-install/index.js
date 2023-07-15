069168722c4046a852dbcff00f79d330cf5df225
Tommy Kovalchuk <tommy-pk17@outlook.com>
2023-07-15 07:23:38 -0400

    fix postinstall script

--- post-install/index.js
+++ post-install/index.js
@@ -1,7 +1,6 @@
 const saveFile = require('fs').writeFileSync;
 
-const pkgJsonPath = require.main.paths[0].split('node_modules')[0] + 'package.json';
-
+const pkgJsonPath = process.cwd().split('node_modules')[0] + '/package.json';
 const json = require(pkgJsonPath);
 
 if (!json.hasOwnProperty('scripts')) {
